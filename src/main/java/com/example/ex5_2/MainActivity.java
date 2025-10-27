f42097e9981c0ce0715b09ff0ca064c0b8e3d343
성준민 <hgf8489@gmail.com>
2025-10-27 21:14:35 +0900

    first commit test

---
+++ src/main/java/com/example/ex5_2/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.ex5_2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1, et2, et3;
+    String s1, s2,s3,s4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        et1 = findViewById(R.id.editText1);
+        et2 = findViewById(R.id.editText2);
+        et3 = findViewById(R.id.editText3);
+
+        RadioGroup rg1 = findViewById(R.id.radioGroup);
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButton){
+                    s1 = "남자";
+                }else{
+                    s1 = "여자";
+                }
+            }
+        });
+
+        CheckBox cb1 = findViewById(R.id.checkBox);
+        CheckBox cb2 = findViewById(R.id.checkBox2);
+        CheckBox cb3 = findViewById(R.id.checkBox3);
+
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    s2 = "소설 ";
+                }
+            }
+
+        });
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    s3 = "비소설 ";
+                }
+            }
+
+        });
+       cb3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    s4 = "컴퓨터";
+                }
+            }
+
+        });
+
+    }
+
+    public void onButtonClick(View view) {
+        String result = "이름 : " + et1.getText().toString() + "\n" +
+                "전화번호 : " + et2.getText().toString() + "\n" +
+                "성별 : " + s1 + "\n" +
+                "관심도서 : " + s2+s3+s4 + "\n";
+        et3.setText(result);
+    }
+}
